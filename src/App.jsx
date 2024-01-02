5bd809052f54c6d1224d3421c2fd25cbeae08dd3
Jonathan Z <82778590+Zkabwang@users.noreply.github.com>
2024-01-02 11:39:22 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import About from './components/About';
+import Hero from './components/Hero';
+import NavBar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills'
+
+
+function App() {
+  return (
+
+    <>
+      <NavBar />
+      <Hero />
+      <Skills />
+      <About />
+      < Projects />
+
+    </>
+
+  );
+}
+
+export default App;
